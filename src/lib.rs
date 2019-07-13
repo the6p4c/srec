1a5c109ac80c6379a1daccbf444abdaa6fe701e7
The6P4C <watsonjcampbell@gmail.com>
2019-07-13 23:37:21 +1000

    Add Image struct to manage blocks of data

--- src/lib.rs
+++ src/lib.rs
@@ -69,6 +69,60 @@ fn checksum_of(data: &Vec<u8>) -> u8 {
     !data.iter().map(|b| Wrapping(*b)).sum::<Wrapping<u8>>().0
 }
 
+#[derive(Debug, PartialEq)]
+struct Block {
+    address: u32,
+    data: Vec<u8>,
+}
+
+#[derive(Debug, PartialEq)]
+struct Image {
+    blocks: Vec<Block>,
+}
+
+impl Image {
+    fn new() -> Image {
+        Image { blocks: vec![] }
+    }
+
+    fn add_data(&mut self, address: u32, data: &Vec<u8>) {
+        self.blocks.push(Block {
+            address: address,
+            data: data.clone(),
+        });
+        self.blocks
+            .sort_unstable_by(|a, b| a.address.cmp(&b.address));
+
+        loop {
+            let pair = {
+                let blocks_first = self.blocks.iter().enumerate();
+                let blocks_last = self.blocks.iter().enumerate().skip(1);
+                let mut contiguous_pairs =
+                    blocks_first
+                        .zip(blocks_last)
+                        .filter(|((_, first), (_, last))| {
+                            let first_first_address_after =
+                                first.address + (first.data.len() as u32);
+                            let blocks_are_contiguous = first_first_address_after == last.address;
+
+                            blocks_are_contiguous
+                        });
+
+                contiguous_pairs.next()
+            };
+
+            if let Some(((i_first, _first), (i_last, last))) = pair {
+                let last_data = last.data.clone();
+                self.blocks[i_first].data.extend(last_data);
+                self.blocks.remove(i_last);
+                continue;
+            } else {
+                break;
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -149,5 +203,113 @@ mod tests {
         assert_eq!(checksum_of(&vec![0x03, 0x00, 0x03]), 0xF9);
 
         assert_eq!(checksum_of(&vec![0x03, 0x00, 0x00]), 0xFC);
+    }
+
+    #[test]
+    fn image_add_data_allocates_new_block() {
+        let mut i = Image::new();
+
+        i.add_data(0x00000000, &vec![0x11, 0x22, 0x33, 0x44]);
+
+        assert_eq!(
+            i.blocks,
+            vec![Block {
+                address: 0x00000000,
+                data: vec![0x11, 0x22, 0x33, 0x44]
+            }]
+        );
+    }
+
+    #[test]
+    fn image_add_data_non_contiguous_after_allocates_new_block() {
+        let mut i = Image::new();
+
+        i.add_data(0x00000000, &vec![0x11, 0x22, 0x33, 0x44]);
+        i.add_data(0x00000005, &vec![0x66, 0x77, 0x88, 0x99]);
+
+        assert_eq!(
+            i.blocks,
+            vec![
+                Block {
+                    address: 0x00000000,
+                    data: vec![0x11, 0x22, 0x33, 0x44]
+                },
+                Block {
+                    address: 0x00000005,
+                    data: vec![0x66, 0x77, 0x88, 0x99],
+                }
+            ]
+        );
+    }
+
+    #[test]
+    fn image_add_data_non_contiguous_before_allocates_new_block() {
+        let mut i = Image::new();
+
+        i.add_data(0x00000005, &vec![0x66, 0x77, 0x88, 0x99]);
+        i.add_data(0x00000000, &vec![0x11, 0x22, 0x33, 0x44]);
+
+        assert_eq!(
+            i.blocks,
+            vec![
+                Block {
+                    address: 0x00000000,
+                    data: vec![0x11, 0x22, 0x33, 0x44]
+                },
+                Block {
+                    address: 0x00000005,
+                    data: vec![0x66, 0x77, 0x88, 0x99],
+                }
+            ]
+        );
+    }
+
+    #[test]
+    fn image_add_data_contiguous_after_merged() {
+        let mut i = Image::new();
+
+        i.add_data(0x00000000, &vec![0x11, 0x22, 0x33, 0x44]);
+        i.add_data(0x00000004, &vec![0x55, 0x66, 0x77, 0x88]);
+
+        assert_eq!(
+            i.blocks,
+            vec![Block {
+                address: 0x00000000,
+                data: vec![0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88]
+            }]
+        );
+    }
+
+    #[test]
+    fn image_add_data_contiguous_before_merged() {
+        let mut i = Image::new();
+
+        i.add_data(0x00000004, &vec![0x55, 0x66, 0x77, 0x88]);
+        i.add_data(0x00000000, &vec![0x11, 0x22, 0x33, 0x44]);
+
+        assert_eq!(
+            i.blocks,
+            vec![Block {
+                address: 0x00000000,
+                data: vec![0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88]
+            }]
+        );
+    }
+
+    #[test]
+    fn image_add_data_contiguous_middle_merged() {
+        let mut i = Image::new();
+
+        i.add_data(0x00000000, &vec![0x11, 0x22, 0x33, 0x44]);
+        i.add_data(0x00000005, &vec![0x66, 0x77, 0x88, 0x99]);
+        i.add_data(0x00000004, &vec![0x55]);
+
+        assert_eq!(
+            i.blocks,
+            vec![Block {
+                address: 0x00000000,
+                data: vec![0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99]
+            }]
+        );
     }
 }
